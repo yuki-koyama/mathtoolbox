884d93e3c6c32acc869bb7a00eb7cd02bf1fb1e6
Yuki Koyama <yuki@koyama.xyz>
2018-04-22 20:07:19 +0900

    Add example app for rbf-interpolation and close #3

---
+++ examples/rbf-interpolation/main.cpp
@@ -0,0 +1,57 @@
+#include <vector>
+#include <random>
+#include <iostream>
+#include <Eigen/Core>
+#include <mathtoolbox/rbf-interpolation.hpp>
+
+using Eigen::VectorXd;
+using Eigen::Vector2d;
+using Eigen::MatrixXd;
+
+namespace
+{
+    std::random_device seed;
+    std::default_random_engine engine(seed());
+    std::uniform_real_distribution<double> uniform_dist(-1.0, 1.0);
+    
+    double CalculateFunction(const Vector2d& x)
+    {
+        return std::sin(10.0 * x(0)) + std::sin(10.0 * x(1));
+    }
+}
+
+int main()
+{
+    // Generate scattered data (in this case, 500 data points in a 2-dimensional space)
+    constexpr int    number_of_samples = 500;
+    constexpr double noise_intensity   = 0.1;
+    Eigen::MatrixXd X(2, number_of_samples);
+    Eigen::VectorXd y(number_of_samples);
+    for (int i = 0; i < number_of_samples; ++ i)
+    {
+        X.col(i) = Vector2d(uniform_dist(engine), uniform_dist(engine));
+        y(i)     = CalculateFunction(X.col(i)) + noise_intensity * uniform_dist(engine);
+    }
+
+    // Instantiate the interpolation object
+    mathtoolbox::RbfInterpolation rbf_interpolation(mathtoolbox::RbfType::BiharmonicSpline);
+   
+    // Set data
+    rbf_interpolation.SetData(X, y);
+    
+    // Compute internal weights with or without regularization
+    constexpr bool use_regularization = true;
+    rbf_interpolation.ComputeWeights(use_regularization);
+    
+    // Calculate and print interpolated values on randomly sampled points
+    std::cout << "x(0),x(1),y" << std::endl;
+    for (int i = 0; i < 100; ++ i)
+    {
+        const Vector2d x = Vector2d(uniform_dist(engine), uniform_dist(engine));
+        const double   y = rbf_interpolation.GetValue(x);
+        
+        std::cout << x(0) << "," << x(1) << "," << y << std::endl;
+    }
+    
+    return 0;
+}

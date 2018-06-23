3e32f9d7812428e074a7ddcb9be9946f7899e918
Yuki Koyama <yuki@koyama.xyz>
2018-06-23 18:08:05 +0900

    Change the test setting to a simple 1D setting

--- examples/gaussian-process-regression/main.cpp
+++ examples/gaussian-process-regression/main.cpp
@@ -5,46 +5,45 @@
 #include <mathtoolbox/gaussian-process-regression.hpp>
 
 using Eigen::VectorXd;
-using Eigen::Vector2d;
 using Eigen::MatrixXd;
 
 namespace
 {
     std::random_device seed;
     std::default_random_engine engine(seed());
-    std::uniform_real_distribution<double> uniform_dist(-1.0, 1.0);
+    std::uniform_real_distribution<double> uniform_dist(0.0, 1.0);
     
-    double CalculateFunction(const Vector2d& x)
+    double CalculateFunction(double x)
     {
-        return std::sin(10.0 * x(0)) + std::sin(10.0 * x(1));
+        return x * std::sin(10.0 * x);
     }
 }
 
 int main()
 {
-    // Generate scattered data (in this case, 500 data points in a 2-dimensional space)
-    constexpr int    number_of_samples = 500;
-    constexpr double noise_intensity   = 0.1;
-    Eigen::MatrixXd X(2, number_of_samples);
+    // Generate scattered data
+    constexpr int    number_of_samples = 10;
+    constexpr double noise_intensity   = 0.10;
+    Eigen::MatrixXd X(1, number_of_samples);
     Eigen::VectorXd y(number_of_samples);
     for (int i = 0; i < number_of_samples; ++ i)
     {
-        X.col(i) = Vector2d(uniform_dist(engine), uniform_dist(engine));
-        y(i)     = CalculateFunction(X.col(i)) + noise_intensity * uniform_dist(engine);
+        X(0, i) = uniform_dist(engine);
+        y(i)    = CalculateFunction(X(0, i)) + noise_intensity * uniform_dist(engine);
     }
     
     // Instantiate the interpolation object
     mathtoolbox::GaussianProcessRegression regressor(X, y);
     
-    // Calculate and print interpolated values on randomly sampled points
-    std::cout << "x(0),x(1),y,s" << std::endl;
-    for (int i = 0; i < 100; ++ i)
+    // Calculate and print estimated values
+    std::cout << "x,y,s" << std::endl;
+    for (int i = 0; i <= 100; ++ i)
     {
-        const Vector2d x = Vector2d(uniform_dist(engine), uniform_dist(engine));
-        const double   y = regressor.EstimateY(x);
-        const double   s = regressor.EstimateS(x);
+        const double x = (1.0 / 100.0) * i;
+        const double y = regressor.EstimateY(Eigen::VectorXd::Constant(1, x));
+        const double s = regressor.EstimateS(Eigen::VectorXd::Constant(1, x));
 
-        std::cout << x(0) << "," << x(1) << "," << y << "," << s << std::endl;
+        std::cout << x << "," << y << "," << s << std::endl;
     }
     
     return 0;

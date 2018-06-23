264b28d9bdeff45963dcde1748322f9fa45c9561
Yuki Koyama <yuki@koyama.xyz>
2018-06-23 13:40:57 +0900

    Add files and class definitions

---
+++ mathtoolbox/gaussian-process-regression.cpp
@@ -0,0 +1,1 @@
+#include "gaussian-process-regression.hpp"
---
+++ mathtoolbox/gaussian-process-regression.hpp
@@ -0,0 +1,33 @@
+#ifndef gaussian_process_regression_hpp
+#define gaussian_process_regression_hpp
+
+#include <Eigen/Core>
+
+namespace mathtoolbox
+{
+    class GaussianProcessRegression
+    {
+    public:
+        
+        GaussianProcessRegression(const Eigen::MatrixXd& X, const Eigen::VectorXd& y);
+        
+        double EstimateY(const Eigen::VectorXd& x) const;
+        double EstimateS(const Eigen::VectorXd& x) const;
+        
+        // Getter methods
+        const Eigen::MatrixXd& GetX() const { return X; }
+        const Eigen::VectorXd& GetY() const { return y; }
+
+    private:
+        
+        // Data points
+        Eigen::MatrixXd X;
+        Eigen::VectorXd y;
+        
+        // Derivative data
+        Eigen::MatrixXd K;
+        Eigen::MatrixXd K_inv;
+    };
+}
+
+#endif /* gaussian_process_regression_hpp */

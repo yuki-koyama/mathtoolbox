08f25403d6305ddc75b3513e48043ba50a76e6df
Yuki Koyama <yuki@koyama.xyz>
2019-03-22 00:25:33 +0900

    Update optimization-test-functions version and set a test function

--- examples/bfgs/main.cpp
+++ examples/bfgs/main.cpp
@@ -1,11 +1,23 @@
+#include <iostream>
 #include <Eigen/Core>
 #include <mathtoolbox/bfgs.hpp>
 #include <optimization-test-functions.hpp>
 
-using Eigen::VectorXd;
-using Eigen::MatrixXd;
-
 int main()
 {
+    mathtoolbox::Setting setting;
+    setting.f = [](const Eigen::VectorXd& x)
+    {
+        return otf::GetValue(x, otf::FunctionType::Sphere);
+    };
+    setting.f_grad = [](const Eigen::VectorXd& x)
+    {
+        return otf::GetGrad(x, otf::FunctionType::Sphere);
+    };
+
+    const mathtoolbox::Result result = mathtoolbox::RunOptimization(setting);
+
+    std::cout << "solution: " << result.x_star << std::endl;
+
     return 0;
 }

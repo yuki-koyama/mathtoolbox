1e44f2007150e1096d9930be0994af92cd5ced85
Yuki Koyama <yuki@koyama.xyz>
2019-03-22 20:03:58 +0900

    Add some equations with some dummy values

--- src/bfgs.cpp
+++ src/bfgs.cpp
@@ -9,20 +9,32 @@ namespace mathtoolbox
 
         const unsigned dim = input.x_init.rows();
 
-        const Eigen::MatrixXd H_init = Eigen::MatrixXd::Identity(dim, dim);
+        const Eigen::MatrixXd H_init = Eigen::MatrixXd::Identity(dim, dim); // TODO
 
         Eigen::MatrixXd H = H_init;
         Eigen::VectorXd x = input.x_init;
+        Eigen::VectorXd grad = input.f_grad(x);
 
         unsigned counter = 0;
         while (true)
         {
-            const Eigen::VectorXd grad = input.f_grad(x);
 
             if (grad.norm() < epsilon || counter == num_max_iterations)
             {
                 break;
             }
+
+            const Eigen::VectorXd p = - H * grad;
+            const double alpha = 0.0; // TODO
+            const Eigen::VectorXd x_next = x + alpha * p;
+            const Eigen::VectorXd s = x_next - x;
+            const Eigen::VectorXd grad_next = input.f_grad(x_next);
+            const Eigen::VectorXd y = grad_next - grad;
+
+            H = H; // TODO
+
+            x = x_next;
+            grad = grad_next;
 
             ++ counter;
         }

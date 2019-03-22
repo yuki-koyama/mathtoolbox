ddd298cf00ac00d79ea128cd0603d47c9b4768b6
Yuki Koyama <yuki@koyama.xyz>
2019-03-22 09:27:00 +0900

    Update otf version and add support of reporting result

--- examples/bfgs/main.cpp
+++ examples/bfgs/main.cpp
@@ -5,19 +5,26 @@
 
 int main()
 {
+    constexpr otf::FunctionType type = otf::FunctionType::Sphere;
+    constexpr int dimensions = 3;
+
     mathtoolbox::Setting setting;
+    setting.x_init = Eigen::VectorXd::Ones(dimensions);
     setting.f = [](const Eigen::VectorXd& x)
     {
-        return otf::GetValue(x, otf::FunctionType::Sphere);
+        return otf::GetValue(x, type);
     };
     setting.f_grad = [](const Eigen::VectorXd& x)
     {
-        return otf::GetGrad(x, otf::FunctionType::Sphere);
+        return otf::GetGrad(x, type);
     };
 
     const mathtoolbox::Result result = mathtoolbox::RunOptimization(setting);
 
-    std::cout << "solution: " << result.x_star << std::endl;
+    const Eigen::VectorXd expected_solution = otf::GetSolution(dimensions, type);
+
+    std::cout << "Found solution: " << result.x_star.transpose() << " (" << result.y_star << ")" << std::endl;
+    std::cout << "Expected solution: " << expected_solution.transpose() << " (" << otf::GetValue(expected_solution, type) << ")" << std::endl;
 
     return 0;
 }

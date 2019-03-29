e5b8c750bd500521906cee20c92d76fb42f807c8
Yuki Koyama <yuki@koyama.xyz>
2019-03-30 00:55:28 +0900

    Add new files

---
+++ include/mathtoolbox/backtracking-line-search.hpp
@@ -0,0 +1,12 @@
+//
+//  backtracking-line-search.hpp
+//  mathtoolbox
+//
+//  Created by Yuki Koyama on 2019/03/30.
+//
+
+#ifndef backtracking_line_search_h
+#define backtracking_line_search_h
+
+
+#endif /* backtracking_line_search_h */
---
+++ include/mathtoolbox/strong-wolfe-conditions-line-search.hpp
@@ -0,0 +1,12 @@
+//
+//  strong-wolfe-conditions-line-search.hpp
+//  mathtoolbox
+//
+//  Created by Yuki Koyama on 2019/03/30.
+//
+
+#ifndef strong_wolfe_conditions_line_search_h
+#define strong_wolfe_conditions_line_search_h
+
+
+#endif /* strong_wolfe_conditions_line_search_h */

ec7a0e1488169ccb76c5717d43a58988aa1ba638
Yuki Koyama <yuki@koyama.xyz>
2019-07-07 15:08:44 +0900

    Remove test codes

--- src/python-bindings.cpp
+++ src/python-bindings.cpp
@@ -2,11 +2,6 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
 
-int add(int i, int j)
-{
-    return i + j;
-}
-
 PYBIND11_MODULE(pymathtoolbox, m)
 {
     m.doc() = "mathtoolbox python bindings";

1a81c374bb6ad5b5e3d4207af630c4b1b24e9c9a
Yuki Koyama <yuki@koyama.xyz>
2019-07-27 00:01:38 +0900

    Add log of log normal and its derivative

--- include/mathtoolbox/distributions.hpp
+++ include/mathtoolbox/distributions.hpp
@@ -3,8 +3,17 @@
 
 namespace mathtoolbox
 {
+    // N(x | mu, sigma^2)
     double GetNormalDist(const double x, const double mu, const double sigma_2);
+
+    // d/dx N(x | mu, sigma^2)
     double GetNormalDistDerivative(const double x, const double mu, const double sigma_2);
+
+    // log{ LogNormal(x | mu, sigma^2) }
+    double GetLogOfLogNormalDist(const double x, const double mu, const double sigma_2);
+
+    // d/dx log{ LogNormal(x | mu, sigma^2) }
+    double GetLogOfLogNormalDistDerivative(const double x, const double mu, const double sigma_2);
 } // namespace mathtoolbox
 
 #endif /* DISTRIBUTIONS_HPP */

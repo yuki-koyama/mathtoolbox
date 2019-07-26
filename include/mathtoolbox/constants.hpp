0b315ce764e665cf4a6a2718ef81fd64be57eff9
Yuki Koyama <yuki@koyama.xyz>
2019-07-26 19:50:19 +0900

    Add a new header file to define constant values

---
+++ include/mathtoolbox/constants.hpp
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+namespace mathtoolbox
+{
+    namespace constants
+    {
+        constexpr double pi = 3.14159265358979323846264338327950288;
+    }
+} // namespace mathtoolbox
+
+#endif /* CONSTANTS_HPP */

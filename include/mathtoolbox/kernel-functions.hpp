51b5e5038a061c790ea705eb15104caa1bc80d02
Yuki Koyama <yuki@koyama.xyz>
2019-07-28 12:56:35 +0900

    Add the ARD squared exponential kernel definitions

--- include/mathtoolbox/kernel-functions.hpp
+++ include/mathtoolbox/kernel-functions.hpp
@@ -1,8 +1,17 @@
 #ifndef KERNEL_FUNCTIONS_HPP
 #define KERNEL_FUNCTIONS_HPP
 
+#include <Eigen/Core>
+
 namespace mathtoolbox
 {
+    double GetArdSquaredExponentialKernel(const Eigen::VectorXd& x_a,
+                                          const Eigen::VectorXd& x_b,
+                                          const Eigen::VectorXd& hyperparameters);
+
+    Eigen::VectorXd GetArdSquaredExponentialKernelHyperparametersDerivative(const Eigen::VectorXd& x_a,
+                                                                            const Eigen::VectorXd& x_b,
+                                                                            const Eigen::VectorXd& hyperparameters);
 } // namespace mathtoolbox
 
 #endif /* KERNEL_FUNCTIONS_HPP */

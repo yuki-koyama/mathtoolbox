30b00dfae09209ad24400e3203877fde9fb1a75d
Yuki Koyama <yuki@koyama.xyz>
2019-07-29 12:13:45 +0900

    Add kernel type definitions

--- include/mathtoolbox/kernel-functions.hpp
+++ include/mathtoolbox/kernel-functions.hpp
@@ -2,9 +2,16 @@
 #define KERNEL_FUNCTIONS_HPP
 
 #include <Eigen/Core>
+#include <functional>
 
 namespace mathtoolbox
 {
+    using Kernel = std::function<double(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&)>;
+    using KernelThetaDerivative =
+        std::function<Eigen::VectorXd(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&)>;
+    using KernelThetaIDerivative =
+        std::function<double(const Eigen::VectorXd&, const Eigen::VectorXd&, const Eigen::VectorXd&, const int)>;
+
     double GetArdSquaredExpKernel(const Eigen::VectorXd& x_a, const Eigen::VectorXd& x_b, const Eigen::VectorXd& theta);
 
     Eigen::VectorXd GetArdSquaredExpKernelThetaDerivative(const Eigen::VectorXd& x_a,

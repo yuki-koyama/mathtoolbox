a42e9c28f3b4eed48366e68ad81526d6e088e09c
Yuki Koyama <yuki@koyama.xyz>
2019-07-30 20:53:51 +0900

    Add derivative of EI

--- include/mathtoolbox/acquisition-functions.hpp
+++ include/mathtoolbox/acquisition-functions.hpp
@@ -10,6 +10,14 @@ namespace mathtoolbox
                                   const std::function<double(const Eigen::VectorXd&)>& mu,
                                   const std::function<double(const Eigen::VectorXd&)>& sigma,
                                   const Eigen::VectorXd&                               x_best);
+
+    Eigen::VectorXd
+    GetExpectedImprovementDerivative(const Eigen::VectorXd&                                        x,
+                                     const std::function<double(const Eigen::VectorXd&)>&          mu,
+                                     const std::function<double(const Eigen::VectorXd&)>&          sigma,
+                                     const Eigen::VectorXd&                                        x_best,
+                                     const std::function<Eigen::VectorXd(const Eigen::VectorXd&)>& mu_derivative,
+                                     const std::function<Eigen::VectorXd(const Eigen::VectorXd&)>& sigma_derivative);
 } // namespace mathtoolbox
 
 #endif /* ACQUISITION_FUNCTIONS_HPP */
--- src/acquisition-functions.cpp
+++ src/acquisition-functions.cpp
@@ -9,15 +9,46 @@ double mathtoolbox::GetExpectedImprovement(const VectorXd&                      
                                            const std::function<double(const VectorXd&)>& sigma,
                                            const VectorXd&                               x_best)
 {
-    const double y_best = mu(x_best);
-    const double s_x    = sigma(x);
-    const double diff   = mu(x) - y_best;
-    const double u      = diff / s_x;
-    const double Phi    = GetStandardNormalDistCdf(u);
-    const double phi    = GetStandardNormalDist(u);
-    const double EI     = diff * Phi + s_x * phi;
+    const double sigma_x = sigma(x);
+    const double f_best  = mu(x_best);
+    const double diff    = mu(x) - f_best;
+    const double Z       = diff / sigma_x;
+    const double Phi     = GetStandardNormalDistCdf(Z);
+    const double phi     = GetStandardNormalDist(Z);
+    const double EI      = diff * Phi + sigma_x * phi;
 
     constexpr double epsilon = 1e-16;
 
-    return (s_x < epsilon || std::isnan(EI)) ? 0.0 : EI;
+    return (sigma_x < epsilon || std::isnan(EI)) ? 0.0 : EI;
+}
+
+VectorXd mathtoolbox::GetExpectedImprovementDerivative(const VectorXd&                                 x,
+                                                       const std::function<double(const VectorXd&)>&   mu,
+                                                       const std::function<double(const VectorXd&)>&   sigma,
+                                                       const VectorXd&                                 x_best,
+                                                       const std::function<VectorXd(const VectorXd&)>& mu_derivative,
+                                                       const std::function<VectorXd(const VectorXd&)>& sigma_derivative)
+{
+    const VectorXd mu_x_derivative    = mu_derivative(x);
+    const VectorXd sigma_x_derivative = sigma_derivative(x);
+
+    const double f_best  = mu(x_best);
+    const double sigma_x = sigma(x);
+    const double mu_x    = mu(x);
+    const double diff    = mu_x - f_best;
+
+    const double Z = diff / sigma_x;
+
+    const double Phi              = GetStandardNormalDistCdf(Z);
+    const double phi              = GetStandardNormalDist(Z);
+    const double phi_Z_derivative = GetStandardNormalDistDerivative(Z);
+
+    const VectorXd Z_x_derivative = (mu_x_derivative - Z * sigma_x_derivative) / sigma_x;
+
+    const VectorXd EI_derivative = mu_x_derivative * Phi + diff * Z_x_derivative * phi + sigma_x_derivative * phi +
+                                   sigma_x * Z_x_derivative * phi_Z_derivative;
+
+    constexpr double epsilon = 1e-16;
+
+    return (sigma_x < epsilon || EI_derivative.hasNaN()) ? VectorXd::Zero(x.size()) : EI_derivative;
 }

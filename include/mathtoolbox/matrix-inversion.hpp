b375679bf002e49e8d3ecd4aeb44d76ebf229b3e
Yuki Koyama <yuki@koyama.xyz>
2019-08-03 12:20:59 +0900

    Add function definition

--- include/mathtoolbox/matrix-inversion.hpp
+++ include/mathtoolbox/matrix-inversion.hpp
@@ -1,8 +1,12 @@
 #ifndef MATHTOOLBOX_MATRIX_INVERSION_HPP
 #define MATHTOOLBOX_MATRIX_INVERSION_HPP
 
+#include <Eigen/Core>
+
 namespace mathtoolbox
 {
+    Eigen::MatrixXd GetInverseUsingUpperLeftBlockInverse(const Eigen::MatrixXd& matrix,
+                                                         const Eigen::MatrixXd& upper_left_block_inverse);
 } // namespace mathtoolbox
 
 #endif /* MATHTOOLBOX_MATRIX_INVERSION_HPP */
--- src/matrix-inversion.cpp
+++ src/matrix-inversion.cpp
@@ -1,1 +1,9 @@
 #include <mathtoolbox/matrix-inversion.hpp>
+
+using Eigen::MatrixXd;
+
+MatrixXd mathtoolbox::GetInverseUsingUpperLeftBlockInverse(const MatrixXd& matrix,
+                                                           const MatrixXd& upper_left_block_inverse)
+{
+    // TODO
+}

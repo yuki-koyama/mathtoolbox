996e9f47d70d568c37557126196393e0a9bf8a5d
Yuki Koyama <yuki@koyama.xyz>
2019-08-03 12:14:34 +0900

    Add new files

---
+++ include/mathtoolbox/matrix-inversion.hpp
@@ -0,0 +1,8 @@
+#ifndef MATHTOOLBOX_MATRIX_INVERSION_HPP
+#define MATHTOOLBOX_MATRIX_INVERSION_HPP
+
+namespace mathtoolbox
+{
+} // namespace mathtoolbox
+
+#endif /* MATHTOOLBOX_MATRIX_INVERSION_HPP */
---
+++ src/matrix-inversion.cpp
@@ -0,0 +1,1 @@
+#include <mathtoolbox/matrix-inversion.hpp>

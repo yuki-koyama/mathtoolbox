3ba94b10346af2b8d8a91c301f3900882f407601
Yuki Koyama <yuki@koyama.xyz>
2019-08-03 22:55:07 +0900

    Update the main code

--- src/matrix-inversion.cpp
+++ src/matrix-inversion.cpp
@@ -8,6 +8,7 @@ MatrixXd mathtoolbox::GetInverseUsingUpperLeftBlockInverse(const MatrixXd& matri
 {
     const int size       = matrix.rows();
     const int block_size = upper_left_block_inverse.rows();
+    const int rest_size  = size - block_size;
 
     assert(block_size > 0);
     assert(size > block_size);
@@ -16,9 +17,9 @@ MatrixXd mathtoolbox::GetInverseUsingUpperLeftBlockInverse(const MatrixXd& matri
     assert(upper_left_block_inverse.cols() == block_size);
 
     const Eigen::MatrixXd& A_inv = upper_left_block_inverse;
-    const Eigen::MatrixXd& B     = matrix.block(0, block_size, block_size, size - block_size);
-    const Eigen::MatrixXd& C     = matrix.block(block_size, 0, size - block_size, block_size);
-    const Eigen::MatrixXd& D     = matrix.block(block_size, block_size, size - block_size, size - block_size);
+    const Eigen::MatrixXd& B     = matrix.block(0, block_size, block_size, rest_size);
+    const Eigen::MatrixXd& C     = matrix.block(block_size, 0, rest_size, block_size);
+    const Eigen::MatrixXd& D     = matrix.block(block_size, block_size, rest_size, rest_size);
 
     const Eigen::MatrixXd E     = D - C * A_inv * B;
     const Eigen::MatrixXd E_inv = E.inverse();
@@ -27,10 +28,10 @@ MatrixXd mathtoolbox::GetInverseUsingUpperLeftBlockInverse(const MatrixXd& matri
 
     Eigen::MatrixXd result(size, size);
 
-    result.block(0, 0, block_size, block_size)                                 = A_inv + (A_inv * B) * E_inv * (C * A_inv);
-    result.block(0, block_size, block_size, size - block_size)                 = -A_inv * B * E_inv;
-    result.block(block_size, 0, size - block_size, block_size)                 = -E_inv * C * A_inv;
-    result.block(block_size, block_size, size - block_size, size - block_size) = E_inv;
+    result.block(0, 0, block_size, block_size)                 = A_inv + (A_inv * B) * E_inv * (C * A_inv);
+    result.block(0, block_size, block_size, rest_size)         = -A_inv * B * E_inv;
+    result.block(block_size, 0, rest_size, block_size)         = -E_inv * C * A_inv;
+    result.block(block_size, block_size, rest_size, rest_size) = E_inv;
 
     assert((matrix * result - MatrixXd::Identity(size, size)).cwiseAbs().maxCoeff() < 1e-10);
 

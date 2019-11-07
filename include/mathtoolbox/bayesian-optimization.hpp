67533f49b0826d07ee020baab340738a54fdd767
Yuki Koyama <yuki@koyama.xyz>
2019-11-07 22:39:12 +0900

    Add class-style API

--- include/mathtoolbox/bayesian-optimization.hpp
+++ include/mathtoolbox/bayesian-optimization.hpp
@@ -7,11 +7,45 @@ namespace mathtoolbox
 {
     namespace optimization
     {
+        enum class KernelType
+        {
+            ArdSquaredExp,
+            ArdMatern52
+        };
+
+        // Class-style API
+        class BayesianOptimizer
+        {
+        public:
+            BayesianOptimizer(const std::function<double(const Eigen::VectorXd&)>& f,
+                              const KernelType& kernel_type = KernelType::ArdMatern52);
+
+            /// \brief Perform a single step of the Bayesian optimization algorithm
+            /// \return Newly observed data point
+            Eigen::VectorXd Step();
+
+            /// \brief Calculate the function value
+            /// \return Evaluated function value
+            double EvaluatePoint(const Eigen::VectorXd& x) const { return m_f(x); }
+
+            /// \brief Retrieve the optimizer found so far
+            /// \return Optimizer found so far
+            Eigen::VectorXd GetCurrentOptimizer() const;
+
+        private:
+            const std::function<double(const Eigen::VectorXd&)> m_f;
+
+            Eigen::MatrixXd m_X;
+            Eigen::VectorXd m_y;
+        };
+
+        // Function-style API
         void RunBayesianOptimization(const std::function<double(const Eigen::VectorXd&)>& f,
                                      const unsigned int                                   max_num_iterations,
+                                     const KernelType&                                    kernel_type,
                                      Eigen::VectorXd&                                     x_star,
                                      unsigned int&                                        num_iterations);
-    }
+    } // namespace optimization
 } // namespace mathtoolbox
 
 #endif // MATHTOOLBOX_BAYESIAN_OPTIMIZATION_HPP
--- src/bayesian-optimization.cpp
+++ src/bayesian-optimization.cpp
@@ -1,9 +1,11 @@
 #include <mathtoolbox/bayesian-optimization.hpp>
+#include <mathtoolbox/gaussian-process-regression.hpp>
 
 using Eigen::VectorXd;
 
 void mathtoolbox::optimization::RunBayesianOptimization(const std::function<double(const VectorXd&)>& f,
                                                         const unsigned int max_num_iterations,
+                                                        const KernelType&  kernel_type,
                                                         VectorXd&          x_star,
                                                         unsigned int&      num_iterations)
 {

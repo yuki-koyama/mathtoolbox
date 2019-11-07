ed3a6a7ab1429a1cba0b28246cb383346437317c
Yuki Koyama <yuki@koyama.xyz>
2019-11-07 21:57:57 +0900

    Add API for BO

--- include/mathtoolbox/bayesian-optimization.hpp
+++ include/mathtoolbox/bayesian-optimization.hpp
@@ -1,5 +1,17 @@
 #ifndef MATHTOOLBOX_BAYESIAN_OPTIMIZATION_HPP
 #define MATHTOOLBOX_BAYESIAN_OPTIMIZATION_HPP
 
+#include <Eigen/Core>
 
-#endif /* MATHTOOLBOX_BAYESIAN_OPTIMIZATION_HPP */
+namespace mathtoolbox
+{
+    namespace optimization
+    {
+        void RunBayesianOptimization(const std::function<double(const Eigen::VectorXd&)>& f,
+                                     const unsigned int                                   max_num_iterations,
+                                     Eigen::VectorXd&                                     x_star,
+                                     unsigned int&                                        num_iterations);
+    }
+} // namespace mathtoolbox
+
+#endif // MATHTOOLBOX_BAYESIAN_OPTIMIZATION_HPP
--- src/bayesian-optimization.cpp
+++ src/bayesian-optimization.cpp
@@ -1,1 +1,12 @@
 #include <mathtoolbox/bayesian-optimization.hpp>
+
+using Eigen::VectorXd;
+
+void mathtoolbox::optimization::RunBayesianOptimization(const std::function<double(const VectorXd&)>& f,
+                                                        const unsigned int max_num_iterations,
+                                                        VectorXd&          x_star,
+                                                        unsigned int&      num_iterations)
+{
+    // TODO
+    assert(false);
+}

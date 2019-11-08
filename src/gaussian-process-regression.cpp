41702b805c2605fca0e4895411c71eed9b74a65d
Yuki Koyama <yuki@koyama.xyz>
2019-11-08 19:50:15 +0900

    Unnest namespace

--- src/gaussian-process-regression.cpp
+++ src/gaussian-process-regression.cpp
@@ -204,175 +204,172 @@ namespace
     }
 } // namespace
 
-namespace mathtoolbox
+mathtoolbox::GaussianProcessRegression::GaussianProcessRegression(const MatrixXd&  X,
+                                                                  const VectorXd&  y,
+                                                                  const KernelType kernel_type)
+    : m_X(X), m_y(y)
 {
-    GaussianProcessRegression::GaussianProcessRegression(const MatrixXd&  X,
-                                                         const VectorXd&  y,
-                                                         const KernelType kernel_type)
-        : m_X(X), m_y(y)
+    switch (kernel_type)
     {
-        switch (kernel_type)
+        case KernelType::ArdSquaredExp:
         {
-            case KernelType::ArdSquaredExp:
-            {
-                m_kernel                 = GetArdSquaredExpKernel;
-                m_kernel_deriv_theta_i   = GetArdSquaredExpKernelThetaIDerivative;
-                m_kernel_deriv_first_arg = GetArdSquaredExpKernelFirstArgDerivative;
-                break;
-            }
-            case KernelType::ArdMatern52:
-            {
-                m_kernel                 = GetArdMatern52Kernel;
-                m_kernel_deriv_theta_i   = GetArdMatern52KernelThetaIDerivative;
-                m_kernel_deriv_first_arg = GetArdMatern52KernelFirstArgDerivative;
-                break;
-            }
+            m_kernel                 = GetArdSquaredExpKernel;
+            m_kernel_deriv_theta_i   = GetArdSquaredExpKernelThetaIDerivative;
+            m_kernel_deriv_first_arg = GetArdSquaredExpKernelFirstArgDerivative;
+            break;
         }
-
-        const int D = X.rows();
-
-        SetHyperparams(0.10, 1e-05, VectorXd::Constant(D, 0.10));
+        case KernelType::ArdMatern52:
+        {
+            m_kernel                 = GetArdMatern52Kernel;
+            m_kernel_deriv_theta_i   = GetArdMatern52KernelThetaIDerivative;
+            m_kernel_deriv_first_arg = GetArdMatern52KernelFirstArgDerivative;
+            break;
+        }
     }
 
-    void GaussianProcessRegression::SetHyperparams(double                 sigma_squared_f,
-                                                   double                 sigma_squared_n,
-                                                   const Eigen::VectorXd& length_scales)
-    {
-        this->m_kernel_hyperparams = Concat(sigma_squared_f, length_scales);
-        this->m_sigma_squared_n    = sigma_squared_n;
+    const int D = X.rows();
 
-        m_K_y     = CalcLargeKY(m_X, m_sigma_squared_n, m_kernel_hyperparams, m_kernel);
-        m_K_y_inv = m_K_y.inverse();
-    }
+    SetHyperparams(0.10, 1e-05, VectorXd::Constant(D, 0.10));
+}
 
-    void GaussianProcessRegression::PerformMaximumLikelihood(double                 sigma_squared_f_initial,
-                                                             double                 sigma_squared_n_initial,
-                                                             const Eigen::VectorXd& length_scales_initial)
-    {
-        const int D = m_X.rows();
+void mathtoolbox::GaussianProcessRegression::SetHyperparams(double          sigma_squared_f,
+                                                            double          sigma_squared_n,
+                                                            const VectorXd& length_scales)
+{
+    this->m_kernel_hyperparams = Concat(sigma_squared_f, length_scales);
+    this->m_sigma_squared_n    = sigma_squared_n;
 
-        assert(m_kernel_hyperparams.size() == D + 1);
-        assert(length_scales_initial.rows() == D);
+    m_K_y     = CalcLargeKY(m_X, m_sigma_squared_n, m_kernel_hyperparams, m_kernel);
+    m_K_y_inv = m_K_y.inverse();
+}
 
-        const VectorXd x_initial = [&]() {
-            VectorXd x(D + 2);
-            x(0)            = sigma_squared_f_initial;
-            x(1)            = sigma_squared_n_initial;
-            x.segment(2, D) = length_scales_initial;
-            return x;
-        }();
-        const VectorXd upper = VectorXd::Constant(D + 2, 1e+03);
-        const VectorXd lower = VectorXd::Constant(D + 2, 1e-05);
+void mathtoolbox::GaussianProcessRegression::PerformMaximumLikelihood(double          sigma_squared_f_initial,
+                                                                      double          sigma_squared_n_initial,
+                                                                      const VectorXd& length_scales_initial)
+{
+    const int D = m_X.rows();
 
-        using Data = std::tuple<const MatrixXd&, const VectorXd&>;
-        Data data(m_X, m_y);
+    assert(m_kernel_hyperparams.size() == D + 1);
+    assert(length_scales_initial.rows() == D);
 
-        // Currently, the mathtoolbox does not have any numerical optimization algorithms that support lower- and
-        // upper-bound conditions. The hyperparameters here should always be positive for evaluating the objective
-        // function. Also, they should not be very large values because the covariance matrix becomes difficult to
-        // inverse. To resolve these issues, here, the search variables are encoded using a variant of the logit
-        // function. While this approach does not handle bound conditions in an exact sense, it works in this case.
+    const VectorXd x_initial = [&]() {
+        VectorXd x(D + 2);
+        x(0)            = sigma_squared_f_initial;
+        x(1)            = sigma_squared_n_initial;
+        x.segment(2, D) = length_scales_initial;
+        return x;
+    }();
+    const VectorXd upper = VectorXd::Constant(D + 2, 1e+03);
+    const VectorXd lower = VectorXd::Constant(D + 2, 1e-05);
 
-        const auto sigmoid = [](const double x) { return 1.0 / (1.0 + std::exp(-x)); };
+    using Data = std::tuple<const MatrixXd&, const VectorXd&>;
+    Data data(m_X, m_y);
 
-        const auto logit = [](const double x) { return std::log(x / (1.0 - x)); };
+    // Currently, the mathtoolbox does not have any numerical optimization algorithms that support lower- and
+    // upper-bound conditions. The hyperparameters here should always be positive for evaluating the objective
+    // function. Also, they should not be very large values because the covariance matrix becomes difficult to
+    // inverse. To resolve these issues, here, the search variables are encoded using a variant of the logit
+    // function. While this approach does not handle bound conditions in an exact sense, it works in this case.
 
-        const auto encode_value = [&logit](const double x, const double l, const double u) {
-            return logit((x - l) / (u - l));
-        };
+    const auto sigmoid = [](const double x) { return 1.0 / (1.0 + std::exp(-x)); };
 
-        const auto decode_value = [&sigmoid](const double x, const double l, const double u) {
-            return (u - l) * sigmoid(x) + l;
-        };
+    const auto logit = [](const double x) { return std::log(x / (1.0 - x)); };
 
-        const auto calc_decode_value_deriv = [&sigmoid](const double x, const double l, const double u) {
-            const double s = sigmoid(x);
-            return (u - l) * s * (1.0 - s);
-        };
+    const auto encode_value = [&logit](const double x, const double l, const double u) {
+        return logit((x - l) / (u - l));
+    };
 
-        const auto encode_vector = [&encode_value, &lower, &upper](const VectorXd& x) {
-            auto encoded_x = VectorXd(x.size());
-            for (int i = 0; i < x.size(); ++i)
-            {
-                encoded_x[i] = encode_value(x[i], lower[i], upper[i]);
-            }
-            return encoded_x;
-        };
+    const auto decode_value = [&sigmoid](const double x, const double l, const double u) {
+        return (u - l) * sigmoid(x) + l;
+    };
 
-        const auto decode_vector = [&decode_value, &lower, &upper](const VectorXd& x) {
-            auto decoded_x = VectorXd(x.size());
-            for (int i = 0; i < x.size(); ++i)
-            {
-                decoded_x[i] = decode_value(x[i], lower[i], upper[i]);
-            }
-            return decoded_x;
-        };
+    const auto calc_decode_value_deriv = [&sigmoid](const double x, const double l, const double u) {
+        const double s = sigmoid(x);
+        return (u - l) * s * (1.0 - s);
+    };
 
-        const auto calc_decode_vector_deriv = [&calc_decode_value_deriv, &lower, &upper](const VectorXd& x) {
-            auto grad = VectorXd(x.size());
-            for (int i = 0; i < x.size(); ++i)
-            {
-                grad[i] = calc_decode_value_deriv(x[i], lower[i], upper[i]);
-            }
-            return grad;
-        };
+    const auto encode_vector = [&encode_value, &lower, &upper](const VectorXd& x) {
+        auto encoded_x = VectorXd(x.size());
+        for (int i = 0; i < x.size(); ++i)
+        {
+            encoded_x[i] = encode_value(x[i], lower[i], upper[i]);
+        }
+        return encoded_x;
+    };
 
-        std::function<double(const VectorXd&)> f = [&](const VectorXd& x) -> double {
-            const auto decoded_x = decode_vector(x);
+    const auto decode_vector = [&decode_value, &lower, &upper](const VectorXd& x) {
+        auto decoded_x = VectorXd(x.size());
+        for (int i = 0; i < x.size(); ++i)
+        {
+            decoded_x[i] = decode_value(x[i], lower[i], upper[i]);
+        }
+        return decoded_x;
+    };
 
-            const double   sigma_squared_f = decoded_x[0];
-            const double   sigma_squared_n = decoded_x[1];
-            const VectorXd length_scales   = decoded_x.segment(2, x.size() - 2);
+    const auto calc_decode_vector_deriv = [&calc_decode_value_deriv, &lower, &upper](const VectorXd& x) {
+        auto grad = VectorXd(x.size());
+        for (int i = 0; i < x.size(); ++i)
+        {
+            grad[i] = calc_decode_value_deriv(x[i], lower[i], upper[i]);
+        }
+        return grad;
+    };
 
-            const MatrixXd& X = std::get<0>(data);
-            const VectorXd& y = std::get<1>(data);
+    std::function<double(const VectorXd&)> f = [&](const VectorXd& x) -> double {
+        const auto decoded_x = decode_vector(x);
 
-            const double log_likelihood =
-                CalcLogLikelihood(X, y, sigma_squared_n, Concat(sigma_squared_f, length_scales), m_kernel);
+        const double   sigma_squared_f = decoded_x[0];
+        const double   sigma_squared_n = decoded_x[1];
+        const VectorXd length_scales   = decoded_x.segment(2, x.size() - 2);
 
-            return log_likelihood;
-        };
+        const MatrixXd& X = std::get<0>(data);
+        const VectorXd& y = std::get<1>(data);
 
-        std::function<VectorXd(const VectorXd&)> g = [&](const VectorXd& x) -> VectorXd {
-            const auto decoded_x = decode_vector(x);
+        const double log_likelihood =
+            CalcLogLikelihood(X, y, sigma_squared_n, Concat(sigma_squared_f, length_scales), m_kernel);
 
-            const double   sigma_squared_f = decoded_x[0];
-            const double   sigma_squared_n = decoded_x[1];
-            const VectorXd length_scales   = decoded_x.segment(2, x.size() - 2);
+        return log_likelihood;
+    };
 
-            const MatrixXd& X = std::get<0>(data);
-            const VectorXd& y = std::get<1>(data);
+    std::function<VectorXd(const VectorXd&)> g = [&](const VectorXd& x) -> VectorXd {
+        const auto decoded_x = decode_vector(x);
 
-            const VectorXd log_likelihood_deriv = CalcLogLikelihoodDeriv(
-                X, y, sigma_squared_n, Concat(sigma_squared_f, length_scales), m_kernel, m_kernel_deriv_theta_i);
+        const double   sigma_squared_f = decoded_x[0];
+        const double   sigma_squared_n = decoded_x[1];
+        const VectorXd length_scales   = decoded_x.segment(2, x.size() - 2);
 
-            return (log_likelihood_deriv.array() * calc_decode_vector_deriv(x).array()).matrix();
-        };
+        const MatrixXd& X = std::get<0>(data);
+        const VectorXd& y = std::get<1>(data);
 
-        optimization::Setting input;
-        input.algorithm          = optimization::Algorithm::LBfgs;
-        input.x_init             = encode_vector(x_initial);
-        input.f                  = f;
-        input.g                  = g;
-        input.epsilon            = 1e-06;
-        input.max_num_iterations = 1000;
-        input.type               = optimization::Type::Max;
+        const VectorXd log_likelihood_deriv = CalcLogLikelihoodDeriv(
+            X, y, sigma_squared_n, Concat(sigma_squared_f, length_scales), m_kernel, m_kernel_deriv_theta_i);
 
-        const auto     result    = optimization::RunOptimization(input);
-        const VectorXd x_optimal = decode_vector(result.x_star);
+        return (log_likelihood_deriv.array() * calc_decode_vector_deriv(x).array()).matrix();
+    };
 
-        m_kernel_hyperparams[0]            = x_optimal[0];
-        m_kernel_hyperparams.segment(1, D) = x_optimal.segment(2, D);
-        m_sigma_squared_n                  = x_optimal[1];
+    optimization::Setting input;
+    input.algorithm          = optimization::Algorithm::LBfgs;
+    input.x_init             = encode_vector(x_initial);
+    input.f                  = f;
+    input.g                  = g;
+    input.epsilon            = 1e-06;
+    input.max_num_iterations = 1000;
+    input.type               = optimization::Type::Max;
 
-        std::cout << "sigma_squared_f: " << m_kernel_hyperparams[0] << std::endl;
-        std::cout << "sigma_squared_n: " << m_sigma_squared_n << std::endl;
-        std::cout << "length_scales  : " << m_kernel_hyperparams.segment(1, D).transpose() << std::endl;
+    const auto     result    = optimization::RunOptimization(input);
+    const VectorXd x_optimal = decode_vector(result.x_star);
 
-        m_K_y     = CalcLargeKY(m_X, m_sigma_squared_n, m_kernel_hyperparams, m_kernel);
-        m_K_y_inv = m_K_y.inverse();
-    }
-} // namespace mathtoolbox
+    m_kernel_hyperparams[0]            = x_optimal[0];
+    m_kernel_hyperparams.segment(1, D) = x_optimal.segment(2, D);
+    m_sigma_squared_n                  = x_optimal[1];
+
+    std::cout << "sigma_squared_f: " << m_kernel_hyperparams[0] << std::endl;
+    std::cout << "sigma_squared_n: " << m_sigma_squared_n << std::endl;
+    std::cout << "length_scales  : " << m_kernel_hyperparams.segment(1, D).transpose() << std::endl;
+
+    m_K_y     = CalcLargeKY(m_X, m_sigma_squared_n, m_kernel_hyperparams, m_kernel);
+    m_K_y_inv = m_K_y.inverse();
+}
 
 double mathtoolbox::GaussianProcessRegression::PredictMean(const VectorXd& x) const
 {
@@ -388,13 +385,13 @@ double mathtoolbox::GaussianProcessRegression::PredictStdev(const VectorXd& x) c
     return std::sqrt(k_xx - k.transpose() * (m_K_y_inv * k));
 }
 
-VectorXd mathtoolbox::GaussianProcessRegression::PredictMeanDeriv(const Eigen::VectorXd& x) const
+VectorXd mathtoolbox::GaussianProcessRegression::PredictMeanDeriv(const VectorXd& x) const
 {
     const MatrixXd k_deriv_x = CalcSmallKDerivSmallX(x, m_X, m_kernel_hyperparams, m_kernel_deriv_first_arg);
     return k_deriv_x * (m_K_y_inv * m_y);
 }
 
-VectorXd mathtoolbox::GaussianProcessRegression::PredictStdevDeriv(const Eigen::VectorXd& x) const
+VectorXd mathtoolbox::GaussianProcessRegression::PredictStdevDeriv(const VectorXd& x) const
 {
     const MatrixXd k_deriv_x = CalcSmallKDerivSmallX(x, m_X, m_kernel_hyperparams, m_kernel_deriv_first_arg);
     const VectorXd k         = CalcSmallK(x, m_X, m_kernel_hyperparams, m_kernel);

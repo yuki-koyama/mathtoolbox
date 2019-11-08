365cc755c4a72ffe29468c3c3d7e9bb211a78d2a
Yuki Koyama <yuki@koyama.xyz>
2019-11-09 01:01:12 +0900

    Add implementation body

--- src/gradient-descent.cpp
+++ src/gradient-descent.cpp
@@ -1,1 +1,20 @@
 #include <mathtoolbox/gradient-descent.hpp>
+
+using Eigen::VectorXd;
+
+void mathtoolbox::optimization::RunGradientDescent(const VectorXd&                                 x_init,
+                                                   const std::function<double(const VectorXd&)>&   f,
+                                                   const std::function<VectorXd(const VectorXd&)>& g,
+                                                   const VectorXd&                                 lower_bound,
+                                                   const VectorXd&                                 upper_bound,
+                                                   const double                                    epsilon,
+                                                   const unsigned int                              max_num_iterations,
+                                                   VectorXd&                                       x_star,
+                                                   unsigned int&                                   num_iterations)
+{
+    // TODO
+    assert(false);
+
+    x_star         = x_init;
+    num_iterations = 0;
+}

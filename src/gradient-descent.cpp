58d9bc18def757b40109a5d64a3370bd9464b2c0
Yuki Koyama <yuki@koyama.xyz>
2019-11-09 01:17:49 +0900

    Implement the main routine

--- src/gradient-descent.cpp
+++ src/gradient-descent.cpp
@@ -12,9 +12,34 @@ void mathtoolbox::optimization::RunGradientDescent(const VectorXd&              
                                                    VectorXd&                                       x_star,
                                                    unsigned int&                                   num_iters)
 {
-    // TODO
-    assert(false);
+    constexpr double gamma = 1e-02;
 
-    x_star    = x_init;
-    num_iters = 0;
+    assert(x_init.size() > 0);
+
+    const int num_dims = x_init.size();
+
+    const bool is_lower_bounded = (lower_bound.size() != 0);
+    const bool is_upper_bounded = (upper_bound.size() != 0);
+
+    assert(!is_lower_bounded || x_init.size() == lower_bound.size());
+    assert(!is_upper_bounded || x_init.size() == upper_bound.size());
+
+    for (int iter = 0; iter < max_num_iters; ++iter)
+    {
+        VectorXd x_new = x_star - gamma * g(x_star);
+
+        if (is_lower_bounded)
+        {
+            x_new = x_new.cwiseMax(lower_bound);
+        }
+        if (is_upper_bounded)
+        {
+            x_new = x_new.cwiseMin(upper_bound);
+        }
+
+        // TODO: Check the termination condition
+
+        x_star    = x_new;
+        num_iters = iter + 1;
+    }
 }

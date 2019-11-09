6de13183d02cc6b6bbc18c2d4d61d13329ba4493
Yuki Koyama <yuki@koyama.xyz>
2019-11-09 11:53:46 +0900

    Add more comments

--- include/mathtoolbox/gradient-descent.hpp
+++ include/mathtoolbox/gradient-descent.hpp
@@ -7,7 +7,17 @@ namespace mathtoolbox
 {
     namespace optimization
     {
-        // Run a simple gradient descent
+        /// \brief Run a simple gradient descent to find a local minimizer of the specified function
+        ///
+        /// \details This algorithm uses the Backtracking Line Search algorithm to determine an appropriate step size.
+        ///
+        /// \param lower_bound The lower bound values. If this is a zero-length empty vector, the algorithm just ignores
+        /// the lower bound condition.
+        ///
+        /// \param upper_bound The upper bound values. If this is a zero-length empty vector, the algorithm just ignores
+        /// the upper bound condition.
+        ///
+        /// \param default_alpha The default step size that the algorithm first tries.
         void RunGradientDescent(const Eigen::VectorXd&                                        x_init,
                                 const std::function<double(const Eigen::VectorXd&)>&          f,
                                 const std::function<Eigen::VectorXd(const Eigen::VectorXd&)>& g,

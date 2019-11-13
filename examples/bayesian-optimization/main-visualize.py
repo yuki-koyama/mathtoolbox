bc4d4cd61f94f020a570a38881a96b8ab96db5c2
Yuki Koyama <yuki@koyama.xyz>
2019-11-13 09:53:08 +0900

    Add a function to calculate statistics of data

--- examples/bayesian-optimization/main-visualize.py
+++ examples/bayesian-optimization/main-visualize.py
@@ -15,12 +15,36 @@ def process_args() -> Tuple[str, str]:
 
 
 def read_csv_as_data_frame(path: str) -> pd.DataFrame:
-    data_frame = pd.read_csv(path, header=None)
-    print(data_frame)
+    return pd.read_csv(path, header=None)
+
+
+def calculate_stats(data_frame: pd.DataFrame) -> pd.DataFrame:
+    num_iters = data_frame.shape[0]
+
+    mean_array = []
+    lower_array = []
+    upper_array = []
+
+    for iter in range(num_iters):
+        mean = data_frame.loc[iter].mean()
+        stdev = data_frame.loc[iter].std()
+
+        mean_array.append(mean)
+        lower_array.append(mean - stdev)
+        upper_array.append(mean + stdev)
+
+    return pd.DataFrame({
+        "mean": mean_array,
+        "lower": lower_array,
+        "upper": upper_array,
+    })
 
 
 if __name__ == "__main__":
-    random_csv_path, bo_csv_path = process_args()
+    rand_csv_path, bo_csv_path = process_args()
 
-    random_data_frame = read_csv_as_data_frame(random_csv_path)
+    rand_data_frame = read_csv_as_data_frame(rand_csv_path)
     bo_data_frame = read_csv_as_data_frame(bo_csv_path)
+
+    rand_stats = calculate_stats(rand_data_frame)
+    bo_stats = calculate_stats(bo_data_frame)

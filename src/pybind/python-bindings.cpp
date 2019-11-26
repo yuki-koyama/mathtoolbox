d7486d2f0137a6b52a341c66f10d2219acf45684
Yuki Koyama <yuki@koyama.xyz>
2019-11-26 14:44:31 +0900

    Add a function to set a seed for random

--- src/pybind/python-bindings.cpp
+++ src/pybind/python-bindings.cpp
@@ -1,10 +1,18 @@
+#include <cstdlib>
 #include <mathtoolbox/classical-mds.hpp>
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
 
+void SetSeed(const unsigned seed)
+{
+    srand(seed);
+}
+
 PYBIND11_MODULE(pymathtoolbox, m)
 {
     m.doc() = "mathtoolbox python bindings";
+
+    m.def("set_seed", &SetSeed);
 
     m.def("compute_classical_mds",
           &mathtoolbox::ComputeClassicalMds,

174745bce1807fcb234fcd2664645bde804b4262
Yuki Koyama <yuki@koyama.xyz>
2019-11-28 20:41:39 +0900

    Add a backtracking line search algorithm with a simple bound constraint handling

--- include/mathtoolbox/backtracking-line-search.hpp
+++ include/mathtoolbox/backtracking-line-search.hpp
@@ -25,6 +25,8 @@ namespace mathtoolbox
         {
             constexpr unsigned int max_num_iters = 50;
 
+            assert(p.size() == x.size());
+
             double alpha = alpha_init;
 
             for (int iter = 0; iter < max_num_iters; ++iter)
@@ -33,6 +35,55 @@ namespace mathtoolbox
                 const bool armijo_condition = f(x + alpha * p) <= f(x) + c * alpha * grad.transpose() * p;
 
                 if (armijo_condition)
+                {
+                    return alpha;
+                }
+
+                alpha *= rho;
+            }
+
+            std::cerr << "Warning: The line search did not converge." << std::endl;
+            return alpha;
+        }
+
+        /// \brief Run the backtracking line search with bound constraints
+        ///
+        /// \details This function should be slightly slower than the unbounded one since it evaluates bound conditions
+        /// every step.
+        ///
+        /// \param lower_bound Lower bound of the search space. It can also be a zero-sized vector when there is no
+        /// lower bound condition.
+        ///
+        /// \param upper_bound Upper bound of the search space. It can also be a zero-sized vector when there is no
+        /// upper bound condition.
+        inline double RunBacktrackingBoundedLineSearch(const std::function<double(const Eigen::VectorXd&)>& f,
+                                                       const Eigen::VectorXd&                               grad,
+                                                       const Eigen::VectorXd&                               x,
+                                                       const Eigen::VectorXd&                               p,
+                                                       const Eigen::VectorXd&                               lower_bound,
+                                                       const Eigen::VectorXd&                               upper_bound,
+                                                       const double                                         alpha_init,
+                                                       const double                                         rho,
+                                                       const double                                         c = 1e-04)
+        {
+            constexpr unsigned int max_num_iters = 50;
+
+            assert(p.size() == x.size());
+
+            double alpha = alpha_init;
+
+            for (int iter = 0; iter < max_num_iters; ++iter)
+            {
+                const Eigen::VectorXd x_new = x + alpha * p;
+
+                // Equation 3.4
+                const bool armijo_condition = f(x_new) <= f(x) + c * alpha * grad.transpose() * p;
+
+                // Bound conditions
+                const bool lower_bound_condition = lower_bound.size() == 0 || (x_new - lower_bound).minCoeff() >= 0.0;
+                const bool upper_bound_condition = upper_bound.size() == 0 || (upper_bound - x_new).minCoeff() >= 0.0;
+
+                if (armijo_condition && lower_bound_condition && upper_bound_condition)
                 {
                     return alpha;
                 }

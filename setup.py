c109d25cfa8e868ec621874c5fdb81640730e041
Yuki Koyama <yuki@koyama.xyz>
2019-12-06 09:53:49 +0900

    Edit setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 import setuptools
 
+__version__ = '1.0.dev0'
+
+ext_modules = [
+]
+
 setuptools.setup(
     name="pymathtoolbox",
-    version="1.0.dev0",
+    version=__version__,
     author="Yuki Koyama",
     author_email="yuki@koyama.xyz",
     description="Python bindings of mathtoolbox: mathematical tools (interpolation, dimensionality reduction, optimization, etc.) written in C++11 with Eigen",
     long_description="",
     long_description_content_type="text/markdown",
+    ext_modules=ext_modules,
     url="https://github.com/yuki-koyama/mathtoolbox",
     packages=setuptools.find_packages(),
     classifiers=[

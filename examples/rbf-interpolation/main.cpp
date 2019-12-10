fed81924d79b08dcbb05d08c5102404157dae6da
Yuki Koyama <yuki@koyama.xyz>
2019-12-10 12:53:10 +0900

    Update test

--- examples/rbf-interpolation/main.cpp
+++ examples/rbf-interpolation/main.cpp
@@ -14,7 +14,7 @@ namespace
     std::default_random_engine             engine(seed());
     std::uniform_real_distribution<double> uniform_dist(-1.0, 1.0);
 
-    double CalculateFunction(const Vector2d& x) { return std::sin(10.0 * x(0)) + std::sin(10.0 * x(1)); }
+    double CalcFunction(const Vector2d& x) { return std::sin(10.0 * x(0)) + std::sin(10.0 * x(1)); }
 } // namespace
 
 int main()
@@ -27,17 +27,20 @@ int main()
     for (int i = 0; i < number_of_samples; ++i)
     {
         X.col(i) = Vector2d(uniform_dist(engine), uniform_dist(engine));
-        y(i)     = CalculateFunction(X.col(i)) + noise_intensity * uniform_dist(engine);
+        y(i)     = CalcFunction(X.col(i)) + noise_intensity * uniform_dist(engine);
     }
 
+    // Define interpolation settings
+    const auto     kernel             = std::make_shared<mathtoolbox::ThinPlateSplineRbfKernel>();
+    constexpr bool use_regularization = true;
+
     // Instantiate an interpolator
-    mathtoolbox::RbfInterpolator rbf_interpolator;
+    mathtoolbox::RbfInterpolator rbf_interpolator(kernel);
 
     // Set data
     rbf_interpolator.SetData(X, y);
 
-    // Compute internal weights with or without regularization
-    constexpr bool use_regularization = true;
+    // Calculate internal weights with or without regularization
     rbf_interpolator.CalcWeights(use_regularization);
 
     // Calculate and print interpolated values on randomly sampled points in CSV format

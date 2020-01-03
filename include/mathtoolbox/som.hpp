275517211f0d5f0b4cf7cd97567d08c630202c84
Yuki Koyama <yuki@koyama.xyz>
2020-01-03 17:04:50 +0900

    Update API and add some class members

--- include/mathtoolbox/som.hpp
+++ include/mathtoolbox/som.hpp
@@ -8,10 +8,32 @@ namespace mathtoolbox
     class Som
     {
     public:
-        Som(const Eigen::MatrixXd X);
+        Som(const Eigen::MatrixXd& data,
+            const int              latent_num_dims = 2,
+            const int              resolution      = 10,
+            const bool             normalize_data  = true);
+
+        const Eigen::MatrixXd& GetGrid() const { return m_Y; }
+        const Eigen::MatrixXd& GetEmbedding() const { return m_Z; }
 
     private:
+        const int m_latent_num_dims;
+        const int m_resolution;
+
+        /// \brief Observed data points.
         Eigen::MatrixXd m_X;
+
+        /// \brief Vector values on grid nodes.
+        Eigen::MatrixXd m_Y;
+
+        /// \brief Embeded data points.
+        Eigen::MatrixXd m_Z;
+
+        /// \brief Perform normalization for the current data.
+        void NormalizeData();
+
+        /// \brief Perform initialization.
+        void PerformInitialization();
     };
 } // namespace mathtoolbox
 
--- src/som.cpp
+++ src/som.cpp
@@ -1,3 +1,27 @@
 #include <mathtoolbox/som.hpp>
 
-mathtoolbox::Som::Som(const Eigen::MatrixXd X) : m_X(X) {}
+mathtoolbox::Som::Som(const Eigen::MatrixXd& m_data,
+                      const int              latent_num_dims,
+                      const int              resolution,
+                      const bool             normalize_data)
+    : m_latent_num_dims(latent_num_dims), m_resolution(resolution), m_X(m_data)
+{
+    if (normalize_data)
+    {
+        this->NormalizeData();
+    }
+
+    this->PerformInitialization();
+}
+
+void mathtoolbox::Som::NormalizeData()
+{
+    // TODO: Implement this function
+    assert(false);
+}
+
+void mathtoolbox::Som::PerformInitialization()
+{
+    // TODO: Implement this function
+    assert(false);
+}

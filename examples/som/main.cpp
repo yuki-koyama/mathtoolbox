86304676bf60a2c254fa6496d538308c9224cf1a
Yuki Koyama <yuki@koyama.xyz>
2020-01-06 23:01:37 +0900

    Update SOM test

--- examples/som/main.cpp
+++ examples/som/main.cpp
@@ -2,5 +2,15 @@
 
 int main()
 {
+    constexpr int  num_data_dims   = 3;
+    constexpr int  num_points      = 10;
+    constexpr int  num_latent_dims = 2;
+    constexpr int  resolution      = 10;
+    constexpr bool normalize_data  = false;
+
+    Eigen::MatrixXd data(num_data_dims, num_points);
+
+    mathtoolbox::Som som(data, num_latent_dims, resolution, normalize_data);
+
     return 0;
 }

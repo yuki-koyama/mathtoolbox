996b0eb4bf52d775675a64567f2b7b7eb4d749b9
Yuki Koyama <yuki@koyama.xyz>
2020-03-02 20:01:18 +0900

    Add comments and close #71

--- include/mathtoolbox/acquisition-functions.hpp
+++ include/mathtoolbox/acquisition-functions.hpp
@@ -19,11 +19,19 @@ namespace mathtoolbox
                                      const std::function<Eigen::VectorXd(const Eigen::VectorXd&)>& mu_derivative,
                                      const std::function<Eigen::VectorXd(const Eigen::VectorXd&)>& sigma_derivative);
 
+    /// \param hyperparam The hyperparameter that controls the trade-off of exploration and exploitation. Specifically,
+    /// this hyperparameter corresponds to the square root of the beta in [Srinivas et al. ICML '10]. Setting this to
+    /// zero means pure exploitation, and setting to a very large value means (almost) pure exploration. This value
+    /// needs to be non-negative.
     double GetGaussianProcessUpperConfidenceBound(const Eigen::VectorXd&                               x,
                                                   const std::function<double(const Eigen::VectorXd&)>& mu,
                                                   const std::function<double(const Eigen::VectorXd&)>& sigma,
                                                   const double                                         hyperparam);
 
+    /// \param hyperparam The hyperparameter that controls the trade-off of exploration and exploitation. Specifically,
+    /// this hyperparameter corresponds to the square root of the beta in [Srinivas et al. ICML '10]. Setting this to
+    /// zero means pure exploitation, and setting to a very large value means (almost) pure exploration. This value
+    /// needs to be non-negative.
     Eigen::VectorXd GetGaussianProcessUpperConfidenceBoundDerivative(
         const Eigen::VectorXd&                                        x,
         const std::function<double(const Eigen::VectorXd&)>&          mu,

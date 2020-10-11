a2d3ce5c837e5e0faa5383a90139f23fca7c2fe5
Yuki Koyama <yuki@koyama.xyz>
2020-10-11 19:07:01 +0900

    Refactor the RBF interpolation test and add support to report the elapsed time

--- examples/rbf-interpolation/main.cpp
+++ examples/rbf-interpolation/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <mathtoolbox/rbf-interpolation.hpp>
 #include <random>
+#include <timer.hpp>
 #include <vector>
 
 using Eigen::MatrixXd;
@@ -15,12 +16,15 @@ namespace
     std::uniform_real_distribution<double> uniform_dist(-1.0, 1.0);
 
     double CalcFunction(const Vector2d& x) { return std::sin(10.0 * x(0)) + std::sin(10.0 * x(1)); }
+
+    std::shared_ptr<timer::Timer> timer_object;
 } // namespace
 
-int main()
+void PerformTest()
 {
-    constexpr int    number_of_samples = 500;
-    constexpr double noise_intensity   = 0.1;
+    constexpr int    number_of_samples      = 500;
+    constexpr int    number_of_test_samples = 100;
+    constexpr double noise_intensity        = 0.1;
 
     // Generate scattered data (in this case, 500 data points in a 2-dimensional space)
     Eigen::MatrixXd X = Eigen::MatrixXd::Random(2, number_of_samples);
@@ -41,18 +45,33 @@ int main()
     rbf_interpolator.SetData(X, y);
 
     // Calculate internal weights with or without regularization
+    timer_object = std::make_shared<timer::Timer>("CalcWeights()");
     rbf_interpolator.CalcWeights(use_regularization);
+    timer_object = nullptr;
 
-    // Calculate and print interpolated values on randomly sampled points in CSV format
-    constexpr int number_of_test_samples = 100;
+    // Calculate interpolated values on randomly sampled points
+    Eigen::MatrixXd X_test = Eigen::MatrixXd::Random(2, number_of_test_samples);
+    Eigen::VectorXd y_test(number_of_test_samples);
+    timer_object = std::make_shared<timer::Timer>("CalcValue() * " + std::to_string(number_of_test_samples));
+    for (int i = 0; i < number_of_test_samples; ++i)
+    {
+        y_test(i) = rbf_interpolator.CalcValue(X_test.col(i));
+    }
+    timer_object = nullptr;
+
+    // Display the results in the CSV format
     std::cout << "x(0),x(1),y" << std::endl;
     for (int i = 0; i < number_of_test_samples; ++i)
     {
-        const auto   x = Vector2d::Random();
-        const double y = rbf_interpolator.CalcValue(x);
-
-        std::cout << x(0) << "," << x(1) << "," << y << std::endl;
+        std::cout << X_test(0, i) << "," << X_test(1, i) << "," << y_test(i) << std::endl;
     }
+
+    return 0;
+}
+
+int main()
+{
+    PerformTest();
 
     return 0;
 }
